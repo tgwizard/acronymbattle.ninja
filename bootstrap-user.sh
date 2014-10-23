224920006770a9b1efe125a5d4b02f8520f37403
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-10-23 21:42:42 +0200

    cd to /vagrant and enable virtualenv bashrc

--- bootstrap-user.sh
+++ bootstrap-user.sh
@@ -3,3 +3,6 @@ set -e
 virtualenv env
 source env/bin/activate
 pip install -r /vagrant/requirements.txt
+
+grep "cd /vagrant" .bashrc > /dev/null || echo "cd /vagrant" >> .bashrc
+grep ". /home/vagrant/env/bin/activate" .bashrc > /dev/null || echo ". /home/vagrant/env/bin/activate" >> .bashrc
